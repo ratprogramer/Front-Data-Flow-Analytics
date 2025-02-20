8b5e4e1c36418eaba7f011bbae467eed27d4add9
Samuel Martinez <samuelmartinezruiz43@gmail.com>
2025-02-20 17:12:36 -0500

    Vlidaciones formulario registro PP

--- src/Atomos/InputTxt/InputTxt.jsx
+++ src/Atomos/InputTxt/InputTxt.jsx
@@ -1,7 +1,38 @@
 import "./InputTxt.css";
 
-export function InputTxt({id, type, placeholder, register, validaciones}){
-    return(
-        <input className="inputTxt-atomo" id={id} type={type} placeholder={placeholder} {...register(id,validaciones)}></input>
-    )
+export function InputTxt({
+  id,
+  type,
+  placeholder,
+  register,
+  validaciones,
+  value,
+  onChange,
+}) {
+  // Desestructura las funciones de RHF
+  const {
+    onChange: rhfOnChange,
+    onBlur,
+    name,
+    ref,
+  } = register(id, validaciones);
+
+  return (
+    <input
+      className="inputTxt-atomo"
+      id={id}
+      name={name}
+      ref={ref}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onBlur={onBlur}
+      onChange={(e) => {
+        // Primero ejecuta tu onChange personalizado (si existe)
+        if (onChange) onChange(e);
+        // Luego llama al onChange de RHF
+        rhfOnChange(e);
+      }}
+    />
+  );
 }
--- src/Moleculas/InputGroup/TxtGroup/TxtGroup.jsx
+++ src/Moleculas/InputGroup/TxtGroup/TxtGroup.jsx
@@ -2,11 +2,11 @@ import { Label } from "../../../Atomos/Label/Label"
 import { InputTxt } from "../../../Atomos/InputTxt/InputTxt"
 import "./TxtGroup.css"
 
-export function TxtGroup({ id, label, type = "text", placeholder, register, validaciones }){
+export function TxtGroup({ id, label, type = "text", placeholder, register, value, validaciones, onChange }){
     return(
         <div className="inputGroup-molecula">
             <Label htmlFor={id} text={label}/>
-            <InputTxt id={id} type={type} placeholder={placeholder} register={register} validaciones={validaciones}/>
+            <InputTxt id={id} type={type} placeholder={placeholder} onChange={onChange} register={register} validaciones={validaciones} value={value}/>
         </div>
     )
 }
