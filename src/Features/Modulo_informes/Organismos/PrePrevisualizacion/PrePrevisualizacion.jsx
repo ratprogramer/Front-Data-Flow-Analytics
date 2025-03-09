a224850f76ba70ee3e0a01e0c5b7905322007e7f
OrichalcosShunnoros <c3146092091@gmail.com>
2025-03-09 17:17:28 -0500

    cards style in pre pre

--- src/Features/Modulo_informes/Organismos/PrePrevisualizacion/PrePrevisualizacion.jsx
+++ src/Features/Modulo_informes/Organismos/PrePrevisualizacion/PrePrevisualizacion.jsx
@@ -10,8 +10,6 @@ export function PrePrevisualizacion() {
   const [loteFilter, setLoteFilter] = useState(""); // Estado para el filtro de lote
 
   const toggleFilters = () => setShwFltrs((prev) => !prev);
-  
-  
   const formatDateToDMY = (dateString) => {
     if (!dateString) return "";
     const date = new Date(dateString);
@@ -163,9 +161,26 @@ export function PrePrevisualizacion() {
         {getFilteredCards().map((card, index) => (
           <div className="crd" key={index}>
             <div className="info">
-              <h3>{card.nombre_pp || card.sabor || "Error al cargar"}</h3>
-              <p>{formatDateToDMY(card.fecha_analisis)}</p>
-              <p>{card.lote}</p>
+              <h3>{card.nombre_pp || card.sabor || "Error al cargar"} 
+                <span>
+                  <svg  
+                    width="24"  
+                    height="24"  
+                    viewBox="0 0 24 24"  
+                    fill="none"  
+                    stroke="currentColor"  
+                    strokeWidth="1"  
+                    strokeLinecap="round"  
+                    strokeLinejoin="round"  
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                      <path d="M9 12l2 2l4 -4" />
+                      </svg>
+                </span>
+              </h3>
+              <p>Fecha de análisis: <span className="sPan">{formatDateToDMY(card.fecha_analisis)}</span></p>
+              <p>Lote: <span className="sPan">{card.lote}</span></p>
             </div>
           </div>
         ))}
