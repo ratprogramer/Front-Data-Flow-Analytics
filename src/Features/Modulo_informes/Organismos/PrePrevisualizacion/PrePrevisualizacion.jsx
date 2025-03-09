f71296532eae0f2a96206a1b2e990838770af946
OrichalcosShunnoros <c3146092091@gmail.com>
2025-03-09 17:46:55 -0500

    sample selected card added to pre pre, (check styles in # end of code css

--- src/Features/Modulo_informes/Organismos/PrePrevisualizacion/PrePrevisualizacion.jsx
+++ src/Features/Modulo_informes/Organismos/PrePrevisualizacion/PrePrevisualizacion.jsx
@@ -162,7 +162,7 @@ export function PrePrevisualizacion() {
           <div className="crd" key={index}>
             <div className="info">
               <h3>{card.nombre_pp || card.sabor || "Error al cargar"} 
-                <span>
+                {/* <span>
                   <svg  
                     width="24"  
                     height="24"  
@@ -177,13 +177,37 @@ export function PrePrevisualizacion() {
                       <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
                       <path d="M9 12l2 2l4 -4" />
                       </svg>
-                </span>
+                </span> */}
               </h3>
               <p>Fecha de análisis: <span className="sPan">{formatDateToDMY(card.fecha_analisis)}</span></p>
               <p>Lote: <span className="sPan">{card.lote}</span></p>
             </div>
           </div>
         ))}
+        <div className="crd" id="crdSlct">
+            <div className="info">
+              <h3>Muestra de ejemplo seleccionada
+                <span>
+                  <svg  
+                    width="28"  
+                    height="28"  
+                    viewBox="0 0 24 24"  
+                    fill="none"  
+                    stroke="currentColor"  
+                    strokeWidth="2"  
+                    strokeLinecap="round"  
+                    strokeLinejoin="round"  
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                      <path d="M9 12l2 2l4 -4" />
+                      </svg>
+                </span>
+              </h3>
+              <p>Fecha de análisis: <span className="sPan">20-03-2024</span></p>
+              <p>Lote: <span className="sPan">12345</span></p>
+            </div>
+          </div>
       </div>
     </div>
   );
