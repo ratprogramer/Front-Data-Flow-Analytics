f6ecf4cf7d723d0af3328a0f51e9f65759a631ea
OrichalcosShunnoros <c3146092091@gmail.com>
2025-03-10 18:45:43 -0500

    commit de algo que no sé donde está

--- src/Features/Modulo_informes/Organismos/PrePrevisualizacion/PrePrevisualizacion.jsx
+++ src/Features/Modulo_informes/Organismos/PrePrevisualizacion/PrePrevisualizacion.jsx
@@ -197,11 +197,11 @@ export function PrePrevisualizacion() {
                     strokeWidth="2"  
                     strokeLinecap="round"  
                     strokeLinejoin="round"  
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-                      <path d="M9 12l2 2l4 -4" />
-                      </svg>
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                    <path d="M9 12l2 2l4 -4" />
+                  </svg>
                 </span>
               </h3>
               <p>Fecha de análisis: <span className="sPan">20-03-2024</span></p>
