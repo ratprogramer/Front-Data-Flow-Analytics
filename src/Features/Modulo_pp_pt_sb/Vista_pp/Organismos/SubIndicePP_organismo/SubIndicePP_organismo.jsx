26a32ee4ddc5b9ca62950dc6cb3320c15e32430e
OrichalcosShunnoros <c3146092091@gmail.com>
2025-03-11 23:15:28 -0500

    rediseño DatosUsuario

--- src/Features/Modulo_pp_pt_sb/Vista_pp/Organismos/SubIndicePP_organismo/SubIndicePP_organismo.jsx
+++ src/Features/Modulo_pp_pt_sb/Vista_pp/Organismos/SubIndicePP_organismo/SubIndicePP_organismo.jsx
@@ -1,9 +1,10 @@
 import { BotonesIndice } from "../../../../../Moleculas/BotonesIndice/BotonesIndice";
 import { TituloPagina } from "../../../../../Moleculas/TituloPagina/TituloPagina";
+import { FilePlus, FileChartColumn } from 'lucide-react';
 export function SubIndicePP_organismo() {
   const botones = [
-    { path: "/ingreso_producto_p", text: "Registrar producto en proceso" },
-    { path: "/registrados_pp", text: "Ingresar resultado " },
+    { path: "/ingreso_producto_p", text: "Registrar producto en proceso", icon: <FilePlus /> },
+    { path: "/registrados_pp", text: "Ingresar resultado ", icon: <FileChartColumn /> },
   ];
   return (
     <div className="Indice-container-organismo">
--- src/Features/Modulo_pp_pt_sb/Vista_pt/Organismos/SubIndicePT_organismo/SubIndicePT_organismo.jsx
+++ src/Features/Modulo_pp_pt_sb/Vista_pt/Organismos/SubIndicePT_organismo/SubIndicePT_organismo.jsx
@@ -1,7 +1,12 @@
 import { BotonesIndice } from "../../../../../Moleculas/BotonesIndice/BotonesIndice";
 import { TituloPagina } from "../../../../../Moleculas/TituloPagina/TituloPagina";
+import { FilePlus, FileChartColumn } from 'lucide-react';
+
 export function SubIndicePT_organismo(){
-    const botones = [{path:"/productos_registrados_pp_", text:"Registrar producto"}, {path:"/productos_registrados_pt", text:"Ingresar resultado"}]
+    const botones = [
+        {path:"/productos_registrados_pp_", text:"Registrar producto", icon:<FilePlus />}, 
+        {path:"/productos_registrados_pt", text:"Ingresar resultado", icon:<FileChartColumn />}
+    ]
     return(
         <div className="Indice-container-organismo">
             <TituloPagina path={"/menu_Derivado_lacteo_fermentado"} text={"Producto terminado"}></TituloPagina>
