c045f75a8af373238c0bf8d1dbe0c5be72441180
OrichalcosShunnoros <c3146092091@gmail.com>
2025-03-17 16:47:33 -0500

    solucionado el problema en sb

--- src/Features/Modulo_pp_pt_sb/Vista_sb/Paginas/FormularioRegistroSB/FormularioRegistroSB_pagina.jsx
+++ src/Features/Modulo_pp_pt_sb/Vista_sb/Paginas/FormularioRegistroSB/FormularioRegistroSB_pagina.jsx
@@ -1,11 +1,13 @@
 import { TituloPagina } from "../../../../../Moleculas/TituloPagina/TituloPagina";
 import { FormularioIngresoSB_organismo } from "../../Organismos/FormularioIngresoSB_organismo/FormularioIngresoSB_organismo";
 
+import "./FormularioRegistroSB_pagina.css";
+
 export function FormularioRegistroSB_pagina() {
   return (
-    <>
+    <div className="FormularioIngresoSB-container-pagina">
       <TituloPagina path={"/menu_Derivado_lacteo_fermentado"} text={"Registro"} />
       <FormularioIngresoSB_organismo />
-    </>
+    </div>
   );
 }
