b12ad029b2e4f676b04bb7c57f98133ea60c128a
Samuel Martinez <samuelmartinezruiz43@gmail.com>
2025-03-18 19:43:59 -0500

    Cierre de sesion con eliminacion de token

--- src/Features/Modulo_menus/Organismos/DatosUsuario/DatosUsuario.jsx
+++ src/Features/Modulo_menus/Organismos/DatosUsuario/DatosUsuario.jsx
@@ -12,7 +12,11 @@ export function DatosUsuario({rol, nombre}){
     const año = fecha.getFullYear();
     const mes = String(fecha.getMonth() + 1).padStart(2, "0"); // Mes inicia en 0, por lo que sumamos 1
     const dia = String(fecha.getDate()).padStart(2, "0");
-    
+
+    const handleNavigate = () => {
+        sessionStorage.setItem("token", "");
+        navigate("/");
+    }
     return(
         <div className="datosUsuario-container">
             <div className="datosUsuario">
@@ -20,11 +24,11 @@ export function DatosUsuario({rol, nombre}){
                 <p><span><UserRound /></span>{ nombre || "Nombre Random" }</p>
                 <p><span><CalendarFold /></span><span className="fecha">Fecha:</span> {año}-{mes}-{dia}</p>
                 
-                <p className="logOut" onClick={() => navigate("/")}>
+                <p className="logOut" onClick={() => handleNavigate()}>
                     <span>
                         <LogOut/>
                     </span>
-                    Log Out
+                    Cerrar sesión
                 </p>
             </div>
         </div>
