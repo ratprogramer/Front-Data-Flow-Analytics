ef90461930a87ca20bbe51872b2bb5bfef64d856
Samuel Martinez <samuelmartinezruiz43@gmail.com>
2025-03-20 15:18:42 -0500

    Cambios en verificaciones de dni y correo

--- src/Features/Modulo_usuarios/Administrador/Menu_princial_admin/Organismos/Reportes/Reportes.jsx
+++ src/Features/Modulo_usuarios/Administrador/Menu_princial_admin/Organismos/Reportes/Reportes.jsx
@@ -5,7 +5,6 @@ export const Reportes = () => {
     <div className="reports-content">
       <h1>Reportes</h1>
       {/* Aquí puedes agregar el contenido relacionado a los reportes */}
-      <p>Aquí iría básicamente, lo de los reportes, algo así como lo del PDF que se hizo para analista.</p>
     </div>
   );
 };
