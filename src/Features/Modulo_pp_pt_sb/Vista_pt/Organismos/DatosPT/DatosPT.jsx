48c6d1e5a815353bbe53561508e493859b1f6d29
OrichalcosShunnoros <c3146092091@gmail.com>
2025-03-20 06:30:10 -0500

    modo oscuro casi completo, falta formularios de registro de resultados

--- src/Features/Modulo_pp_pt_sb/Vista_pt/Organismos/DatosPT/DatosPT.jsx
+++ src/Features/Modulo_pp_pt_sb/Vista_pt/Organismos/DatosPT/DatosPT.jsx
@@ -1,12 +1,14 @@
 import { useLocation } from 'react-router-dom';
 import './DatosPT.css';
+import { useThemeContext } from '../../../../../context/ThemeContext';
 
 export function DatosPT() {
+    const { contextTheme } = useThemeContext();
     const location = useLocation();
     const { nombreMuestra, lote } = location.state || {};
     return (
-        <div className='container-box-datos-pt'>
-            <div className="datosPT-organismo-container">
+        <div className='container-box-datos-pt' id={contextTheme}>
+            <div className="datosPT-organismo-container" id={contextTheme}>
                 <h1 className='datos-pt-txt'>{nombreMuestra}</h1>
                 <p>Lote: {lote}</p>
             </div>
--- src/Features/Modulo_pp_pt_sb/Vista_sb/Organismos/DatosSB/DatosSB.jsx
+++ src/Features/Modulo_pp_pt_sb/Vista_sb/Organismos/DatosSB/DatosSB.jsx
@@ -1,13 +1,15 @@
 import './DatosSB.css';
 import { useLocation } from 'react-router-dom';
+import { useThemeContext } from '../../../../../context/ThemeContext';
 
 export function DatosSB() {
+    const { contextTheme } = useThemeContext();
     const location = useLocation();
     const { sabor, lote } = location.state || {};
     return (
-        <div className='container-box-datos-sb'>
-            <div className="datosSB-organismo-container">
-                <p className="ttPp">{sabor}</p>
+        <div className='container-box-datos-sb' id={contextTheme}>
+            <div className="datosSB-organismo-container" id={contextTheme}>
+                <p className="ttPp" id={contextTheme}>{sabor}</p>
                 <p>Lote: {lote}</p>
             </div>
         </div>
