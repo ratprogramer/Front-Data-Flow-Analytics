a0ecdbf68aaae0563bd1fc0c34dffcaa84892d7c
OrichalcosShunnoros <c3146092091@gmail.com>
2025-03-31 18:32:50 -0500

    estilos aplicados a las tablas de informes

--- src/Features/Modulo_informes/Organismos/PrePrevisualizacion/PrePrevisualizacion.jsx
+++ src/Features/Modulo_informes/Organismos/PrePrevisualizacion/PrePrevisualizacion.jsx
@@ -18,26 +18,54 @@ const styles = StyleSheet.create({
     width: "100%",
     borderStyle: "solid",
     borderWidth: 1,
+    borderColor: "#4bacc6",
+    borderRightWidth: 0,
+    borderBottomWidth: 0,
+    marginBottom: 10,
+  },
+  table2: {
+    display: "table",
+    width: "100%",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderColor: "#4f81bd",
     borderRightWidth: 0,
     borderBottomWidth: 0,
     marginBottom: 10,
   },
   row: { flexDirection: "row" },
   cellHeader: {
+    borderColor: "#4bacc6",
     borderStyle: "solid",
     borderWidth: 1,
     borderLeftWidth: 0,
     borderTopWidth: 0,
     padding: 5,
-    backgroundColor: "#f2f2f2",
+    // backgroundColor: "#f2f2f2",
+    backgroundColor: "#b7dde8",
     fontWeight: "bold",
   },
-  cell: {
+  cellHeader2: {
+    borderColor: "#4f81bd",
     borderStyle: "solid",
     borderWidth: 1,
     borderLeftWidth: 0,
     borderTopWidth: 0,
     padding: 5,
+    backgroundColor: "#4f81bd",
+    fontWeight: "bold",
+  }
+  ,
+  cell: {
+    borderColor: "#4bacc6",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+    padding: 5,
+  },
+  cell2: {
+
   }
 });
 
@@ -95,65 +123,65 @@ export function PrePrevisualizacion() {
     );
   };
 
-const formatDateToDMY = (dateString) => {
-  if (!dateString) return "";
-  const date = new Date(dateString);
-  if (isNaN(date.getTime())) return "";
+  const formatDateToDMY = (dateString) => {
+    if (!dateString) return "";
+    const date = new Date(dateString);
+    if (isNaN(date.getTime())) return "";
 
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const year = date.getFullYear();
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
 
-  return `${day}/${month}/${year}`;
-};
+    return `${day}/${month}/${year}`;
+  };
 
-const renderTable = (title, data, headers, fields) => (
-  <>
-    <Text style={{ textAlign: "center", fontSize: 14, marginVertical: 10, fontWeight: "bold" }}>{title}</Text>
-    <View style={styles.table}>
-    <View style={[styles.row, { backgroundColor: "#ddd" }]}>
-        {headers.map((header, index) => (
-        <Text key={index} style={[styles.cellHeader, { width: `${100 / headers.length}%` }]}>
-            {header}
-        </Text>
-        ))}
-    </View>
-    {data.map((item, index) => (
-        <View key={index} style={styles.row}>
-        {fields.map((field, idx) => (
-            <Text key={idx} style={[styles.cell, { width: `${100 / headers.length}%` }]}>
-            {item[field] || "N/A"}
-            </Text>
-        ))}
-        </View>
-    ))}
-    </View>
-  </>
-);
-
-const renderConformidadTable = (dataLength) => (
-  <>
-    <Text style={{ textAlign: "center", fontSize: 12, marginVertical: 10, fontWeight: "bold" }}>DECLARACIÓN DE CONFORMIDAD</Text>
-    <View style={styles.table}>
+  const renderTable = (title, data, headers, fields) => (
+    <>
+      <Text style={{ textAlign: "center", fontSize: 14, marginVertical: 10, fontWeight: "bold" }}>{title}</Text>
+      <View style={styles.table}>
       <View style={[styles.row, { backgroundColor: "#ddd" }]}>
-        {["Análisis", "n", "c", "Número de muestras fuera de parámetros", "Concepto"].map((header, index) => (
-        <Text key={index} style={[styles.cellHeader, { width: "20%" }]}>
-            {header}
-        </Text>
-        ))}
+          {headers.map((header, index) => (
+          <Text key={index} style={[styles.cellHeader, { width: `${100 / headers.length}%` }]}>
+              {header}
+          </Text>
+          ))}
       </View>
-    {["Coliformes Totales", "Escherichia coli", "Mohos y Levaduras"].map((analysis, index) => (
-      <View key={index} style={styles.row}>
-      <Text style={[styles.cell, { width: "20%" }]}>{analysis}</Text>
-      <Text style={[styles.cell, { width: "20%" }]}>{dataLength}</Text>
-      <Text style={[styles.cell, { width: "20%" }]}>{[2, 0, 2][index]}</Text>
-      <Text style={[styles.cell, { width: "20%" }]}>0</Text>
-      <Text style={[styles.cell, { width: "20%" }]}>CUMPLE</Text>
+      {data.map((item, index) => (
+          <View key={index} style={styles.row}>
+          {fields.map((field, idx) => (
+              <Text key={idx} style={[styles.cell, { width: `${100 / headers.length}%` }]}>
+              {item[field] || "N/A"}
+              </Text>
+          ))}
+          </View>
+      ))}
       </View>
-    ))}
-    </View>
-  </>
-);
+    </>
+  );
+
+  const renderConformidadTable = (dataLength) => (
+    <>
+      <Text style={{ textAlign: "center", fontSize: 12, marginVertical: 10, fontWeight: "bold" }}>DECLARACIÓN DE CONFORMIDAD</Text>
+      <View style={styles.table2}>
+        <View style={[styles.row, { backgroundColor: "#ddd" }]}>
+          {["Análisis", "n", "c", "Número de muestras fuera de parámetros", "Concepto"].map((header, index) => (
+          <Text key={index} style={[styles.cellHeader2, { width: "20%" }]}>
+              {header}
+          </Text>
+          ))}
+        </View>
+      {["Coliformes Totales", "Escherichia coli", "Mohos y Levaduras"].map((analysis, index) => (
+        <View key={index} style={styles.row}>
+        <Text style={[styles.cell, { width: "20%" }]}>{analysis}</Text>
+        <Text style={[styles.cell, { width: "20%" }]}>{dataLength}</Text>
+        <Text style={[styles.cell, { width: "20%" }]}>{[2, 0, 2][index]}</Text>
+        <Text style={[styles.cell, { width: "20%" }]}>0</Text>
+        <Text style={[styles.cell, { width: "20%" }]}>CUMPLE</Text>
+        </View>
+      ))}
+      </View>
+    </>
+  );
 
   const PDFDocument = ({ selectedCards }) => {
   const dataPP = selectedCards
